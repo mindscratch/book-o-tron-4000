2b1ca43f68938cf53c424d94b31831ea642bc975
behmann <behmann@4b19816c-37e1-11de-8daa-cfecb5f7ff5b>
2009-05-12 14:23:29 +0000

    added basic hibernate mapping test

--- bookotron.parent/bookotron.data/src/main/java/bookotron/data/dao/impl/BookDaoImpl.java
+++ bookotron.parent/bookotron.data/src/main/java/bookotron/data/dao/impl/BookDaoImpl.java
@@ -1,17 +1,38 @@
 package bookotron.data.dao.impl;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import bookotron.data.dao.BookDao;
 import bookotron.data.hbm.Book;
 
+@Transactional
 @Repository("bookDao")
 public class BookDaoImpl implements BookDao {
 
+	private SessionFactory sessionFactory;
+	
+	@Autowired
+	BookDaoImpl(SessionFactory sessionFactory) {
+		this.sessionFactory = sessionFactory;
+	}
+	
 	@Override
 	public Book getBookById(Long id) {
-		// TODO Auto-generated method stub
-		return null;
+		Session session = sessionFactory.getCurrentSession();
+		return (Book)session.load(Book.class, id);
 	}
+
+	@Override
+	public Book saveOrUpdate(Book book) {
+		Session session = sessionFactory.getCurrentSession();
+		session.saveOrUpdate(book);
+		return book;
+	}
+	
+	
 
 }
--- bookotron.parent/bookotron.data/src/test/java/bookotron/data/SpringConfigurationTest.java
+++ bookotron.parent/bookotron.data/src/test/java/bookotron/data/SpringConfigurationTest.java
@@ -1,18 +1,29 @@
 package bookotron.data;
 
-import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.AbstractDependencyInjectionSpringContextTests;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import bookotron.data.dao.BookDao;
+import bookotron.data.hbm.Book;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations={"classpath:bookotron-data.xml"})
 public class SpringConfigurationTest extends AbstractDependencyInjectionSpringContextTests {
 
+	@Autowired
+	BookDao bookDao;
+	
 	@Test
-	public void testSpringConfiguration() {
-		Assert.assertTrue(true);
+	public void testBookSave() {
+		Book book = new Book();
+		book.setAuthor("Name");
+		book.setTitle("title");
+		book.setIsbn("1234");
+		bookDao.saveOrUpdate(book);
+		assertNotNull(book.getId());
 	}
 }
