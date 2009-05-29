9272841d0b505e790416372f152e73325628957a
codecraig <codecraig@4b19816c-37e1-11de-8daa-cfecb5f7ff5b>
2009-05-29 11:10:31 +0000

    Beginning book-o-tron data layer, basic DAO in place

---
+++ trunk/bookotron.data/src/main/java/bookotron/data/dao/IBaseDAO.java
@@ -0,0 +1,17 @@
+package bookotron.data.dao;
+
+import java.util.List;
+import java.io.Serializable;
+
+public interface IBaseDAO<T> {
+
+    public T insert(T t);
+
+    public T update(T t);
+
+    public void remove(T t);
+
+    public List<T> findAll(Class<T> objectClass);
+
+    public T find(Class<T> objectClass, Serializable id);
+}
