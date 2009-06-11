6af21f25d219dced746b01c87127b20c65686768
codecraig <codecraig@4b19816c-37e1-11de-8daa-cfecb5f7ff5b>
2009-06-11 02:40:03 +0000

    updated tests and fixed pom so transactions work

--- trunk/bookotron.data/src/test/java/bookotron/data/dao/impl/AddSeedDataTest.java
+++ trunk/bookotron.data/src/test/java/bookotron/data/dao/impl/AddSeedDataTest.java
@@ -6,6 +6,7 @@ import org.junit.After;
 import org.junit.Test;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.transaction.TransactionConfiguration;
+import org.springframework.test.context.transaction.BeforeTransaction;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.AbstractTransactionalDataSourceSpringContextTests;
 import org.springframework.transaction.annotation.Transactional;
@@ -27,6 +28,9 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.io.InputStream;
 import static org.junit.Assert.*;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.log4j.lf5.viewer.LogFactor5LoadingDialog;
 import bookotron.data.dao.IBaseDao;
 import bookotron.data.model.entity.impl.author.Author;
 import bookotron.data.model.entity.impl.user.User;
@@ -34,19 +38,26 @@ import bookotron.model.entity.author.IAuthor;
 import bookotron.model.entity.user.IUser;
 
 @RunWith(SpringJUnit4ClassRunner.class)
+//This annotation tells Spring that we want each method in this class to be
+//executed within it's own transaction.  You can also apply this annotation
+//selectively to each method that needs a transaction, and have other
+//methods execute outside a transaction.
+@Transactional
+//This annotation tells the Spring test runner to always roll back each
+//transaction it creates.
+@TransactionConfiguration(defaultRollback=true)
 @ContextConfiguration(locations = {"classpath:test-context.xml"})
 /**
  * An abstract class which provides support for other DAO tests
  */
 public class AddSeedDataTest {
-
+        Log log = LogFactory.getLog(AddSeedDataTest.class);
     @Autowired
     protected DataSource dbDataSource;
 
     @Autowired
     @Qualifier("authorDao")
     private IBaseDao<IAuthor> dao;
-
 
     @Test
     public void nothing() {
